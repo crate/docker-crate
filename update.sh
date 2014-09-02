8888070d8476370811f9d4ef61ffbbfd86f30e62
Mathias Fussenegger <f.mathias@zignar.net>
2014-09-02 14:03:04 +0200

    add update script and Dockerfile template

---
+++ update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+if [[ -z "$1" ]]; then
+    echo "update.sh <version>"
+    exit 1
+else
+    VERSION=$1
+fi
+
+VERSION_EXISTS=$(curl -f -XHEAD --head https://cdn.crate.io/downloads/releases/crate-${VERSION}.tar.gz)
+
+if [ "$?" != "0" ]; then
+    echo "version $VERSION doesn't exist!"
+    exit 1
+fi
+
+TAG_EXISTS=$(git tag | grep $VERSION)
+
+if [ "$VERSION" == "$TAG_EXISTS" ]
+then
+    echo "Tag $TAG_EXISTS already in use"
+    exit 1
+fi
+
+sed "s/XXX/$VERSION/g" Dockerfile.template > Dockerfile
+git add Dockerfile
+git commit -m "updated Dockerfile to use crate version $VERSION"
+
+git tag $VERSION
+git push --tags
