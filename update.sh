55db322c7eb426166114bc3cd90764bf7312b417
Christian Haudum <christian.haudum@gmail.com>
2017-04-18 17:15:58 +0200

    added  Dockerfile template for CrateDB 1.2.x
    to use new log4j2.properties instead of logging.yml
    
    example usage: `./update.sh 1.2.0 Dockerfile_1_2`

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-
 if [[ -z "$1" ]]; then
-    echo "update.sh <version>"
+    echo "update.sh <version> <template>"
     exit 1
 else
     TAG=$1
+    TEMPLATE=${2:-"Dockerfile"}
     VERSION=`echo $TAG | cut -d '-' -f 1`
 fi
 
@@ -18,12 +18,14 @@ fi
 
 TAG_EXISTS=$(git tag | grep $TAG)
 
-if [ "$TAG" == "$TAG_EXISTS" ]
-then
+if [ "$TAG" == "$TAG_EXISTS" ]; then
     echo "Tag $TAG_EXISTS already in use"
     exit 1
 fi
 
-sed "s/XXX/$VERSION/g" Dockerfile.template > Dockerfile
+if [[ ! -f "$TEMPLATE.template" ]]; then
+    echo "Template $TEMPLATE.template does not exist"
+    exit 1
+fi
 
-
+sed "s/XXX/$VERSION/g" "$TEMPLATE.template" > Dockerfile
