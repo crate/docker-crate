2cc97d38478e70eca39ae0eaad0bbf49043eba10
Christian Haudum <christian.haudum@gmail.com>
2017-12-13 20:54:34 +0100

    make 2nd argument of update.sh script mandatory

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-if [[ -z "$1" ]]; then
+if [[ -z "$1" || -z "$2" ]]; then
     echo "update.sh <version> <template>"
     exit 1
 else
-    TAG=$1
-    TEMPLATE=${2:-"Dockerfile"}
+    TAG="$1"
+    TEMPLATE="$2.template"
     VERSION=`echo $TAG | cut -d '-' -f 1`
 fi
 
@@ -23,9 +23,10 @@ if [ "$TAG" == "$TAG_EXISTS" ]; then
     exit 1
 fi
 
-if [[ ! -f "$TEMPLATE.template" ]]; then
-    echo "Template $TEMPLATE.template does not exist"
+if [[ ! -f "$TEMPLATE" ]]; then
+    echo "Template $TEMPLATE does not exist"
     exit 1
 fi
 
-sed "s/XXX/$VERSION/g" "$TEMPLATE.template" > Dockerfile
+exit 0
+sed "s/XXX/$VERSION/g" "$TEMPLATE" > Dockerfile
