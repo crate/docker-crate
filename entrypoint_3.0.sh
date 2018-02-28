e74b70bf20c9307cd22f5091112789718c241280
Mathias Fussenegger <f.mathias@zignar.net>
2018-02-28 10:37:26 +0100

    Set CRATE_HEAP_DUMP_PATH for CrateDB 3.0+

--- entrypoint_3.0.sh
+++ entrypoint_3.0.sh
@@ -4,6 +4,7 @@ set -ae
 
 # GC logging set to default value of path.logs
 CRATE_GC_LOG_DIR="/data/log"
+CRATE_HEAP_DUMP_PATH="/data/data"
 # Special VM options for Java in Docker
 CRATE_JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Des.cgroups.hierarchy.override=/ $CRATE_JAVA_OPTS"
 
